c804f087888e759f727395ee73e37661543c9237
Marcos <marcos@iovlabs.org>
2021-03-09 10:29:27 -0300

    Include util to decode BTC WIF private keys

--- src/crypto/utils.js
+++ src/crypto/utils.js
@@ -1,4 +1,5 @@
 var jsSHA = require('jssha/sha256');
+var base58 = require('./base58');
 
 function numberToHex (number) {
     var hex = Math.round(number).toString(16);
@@ -26,7 +27,15 @@ function sha256Checksum(payload) {
     return sha256(sha256(payload)).substr(0, 8);
 }
 
+function decodeWifPrivateKey(wifPrivateKey) {
+    let decodedPrivateKey = base58.decode(wifPrivateKey);
+    let privateKey = decodedPrivateKey.slice(1, decodedPrivateKey.length - 5);
+    
+    return toHex(privateKey);
+}
+
 module.exports = {
     toHex,
-    sha256Checksum
+    sha256Checksum,
+    decodeWifPrivateKey
 };
