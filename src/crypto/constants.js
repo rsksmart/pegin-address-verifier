a65364c3fa00f4f54c39415a9418126bc10c79a7
jeremy-then <jeremy.then@iovlabs.org>
2024-01-03 14:06:20 -0400

    Adds createPeginV1TxData function

--- src/crypto/constants.js
+++ src/crypto/constants.js
@@ -8,15 +8,21 @@ const NETWORKS = {
     MAINNET: 'mainnet',
     TESTNET: 'testnet',
     REGTEST: 'regtest'
-}
+};
 
 const HASH_FIELD_NAMES = {
     p2pkh: 'scriptPubKey',
     p2sh: 'scriptHash'
-}
+};
+
+const ADDRESS_TYPES_CODES = {
+    p2pkh: '01',
+    p2sh: '02'
+};
 
 module.exports = {
     ADDRESS_TYPES,
     NETWORKS,
-    HASH_FIELD_NAMES
+    HASH_FIELD_NAMES,
+    ADDRESS_TYPES_CODES,
 };
--- src/pegin-address-verifier.js
+++ src/pegin-address-verifier.js
@@ -1,6 +1,9 @@
-var p2pkhP2sh = require('./crypto/p2pkh-p2sha');
-var segwit = require('./crypto/segwit-addr');
-let { ADDRESS_TYPES } = require('./crypto/constants');
+const p2pkhP2sh = require('./crypto/p2pkh-p2sha');
+const segwit = require('./crypto/segwit-addr');
+const { ADDRESS_TYPES, ADDRESS_TYPES_CODES } = require('./crypto/constants');
+
+const RSKT_PREFIX_HEX = '52534b54'; // 'RSKT' prefix encoded in hex. Check https://github.com/rsksmart/RSKIPs/blob/2c994cc108885ccc5a116e4aee8c073b5eca5682/IPs/RSKIP170.md
+const RSKT_PROTOCOL_VERSION = '01';
 
 module.exports = {
     isValidAddress: function (address, networkType) {
@@ -15,5 +18,36 @@ module.exports = {
         return addressInfo && 
             addressInfo.type == ADDRESS_TYPES.P2PKH || 
             addressInfo.type == ADDRESS_TYPES.P2SH;
+    },
+    createPeginV1TxData: (rskDestinationAddress, btcRefundAddress) => {
+
+        let data = `${RSKT_PREFIX_HEX}${RSKT_PROTOCOL_VERSION}}`;
+        
+        if (rskDestinationAddress.startsWith('0x')) {
+            rskDestinationAddress = rskDestinationAddress.substring(2);
+        }
+
+        data += rskDestinationAddress;
+    
+        if (btcRefundAddress) {
+            const refundAddressInfo = this.getAddressInformation(btcRefundAddress);
+            if (refundAddressInfo) {
+                data += ADDRESS_TYPES_CODES[refundAddressInfo.type];
+                switch (refundAddressInfo.type) {
+                    case 'p2pkh':
+                        data += refundAddressInfo.scriptPubKey;
+                        break;
+                    case 'p2sh':
+                        data += refundAddressInfo.scriptHash;
+                        break;
+                    default:
+                        throw new Error(`Unsupported btc refund address type: ${refundAddressInfo.type}`);
+                }
+            } else {
+                throw new Error(`Could not get address information for ${btcRefundAddress}`);
+            }
+        }
+        
+        return data;
     }
 };
